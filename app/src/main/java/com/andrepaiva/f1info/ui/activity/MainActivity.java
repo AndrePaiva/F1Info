3648345b21d5753e3e759367adf75b4562b188c5
AndrePaiva <andrep3@gmail.com>
2017-05-22 15:59:43 -0300

    Fix portrait screen and define Fedd as first screen

--- app/src/main/java/com/andrepaiva/f1info/ui/activity/MainActivity.java
+++ app/src/main/java/com/andrepaiva/f1info/ui/activity/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        displaySelectedScreen(R.id.nav_calendar);
+        displaySelectedScreen(R.id.nav_dashboard);
     }
 
     @Override
